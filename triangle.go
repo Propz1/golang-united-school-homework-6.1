2fda533c06ee223033a9a186aed42c89fbb63c89
Pavel Zhuravlev <propz@mail.ru>
2022-06-04 13:54:58 -0500

    update the triangle.go file

--- triangle.go
+++ triangle.go
@@ -1,6 +1,18 @@
 package golang_united_school_homework
 
+import "math"
+
 // Triangle must satisfy to Shape interface
 type Triangle struct {
 	Side float64
+}
+
+func (t Triangle) CalcPerimeter() float64 {
+
+	return t.Side * float64(3)
+}
+
+func (t Triangle) CalcArea() float64 {
+
+	return math.Pow(t.Side, float64(2)) * math.Sqrt(3) / 4
 }
