f96ec2c2a0b52c505075fb33f78e578035234b4a
Pavel Zhuravlev <propz@mail.ru>
2022-06-04 14:00:27 -0500

    update the circle.go file

--- circle.go
+++ circle.go
@@ -1,6 +1,22 @@
 package golang_united_school_homework
 
+import "math"
+
+const (
+	pi float64 = math.Pi
+)
+
 // Circle must satisfy to Shape interface
 type Circle struct {
 	Radius float64
+}
+
+func (c Circle) CalcPerimeter() float64 {
+
+	return float64(2) * pi * c.Radius
+}
+
+func (c Circle) CalcArea() float64 {
+
+	return pi * math.Pow(c.Radius, float64(2))
 }
