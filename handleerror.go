0c25f68f38ccf0792ff822bb0d10afa589d3f386
Pavel Zhuravlev <propz@mail.ru>
2022-06-04 01:12:29 -0500

    create handleerror.go file

---
+++ handleerror.go
@@ -0,0 +1,18 @@
+package golang_united_school_homework
+
+type EmptyError struct {
+	Message string
+}
+
+func (m EmptyError) Error() string {
+	return m.Message
+}
+
+func NotEmptyError(err error) bool {
+
+	if _, ok := err.(EmptyError); ok {
+		return false
+	}
+
+	return true
+}
