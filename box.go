7993abe6de19eedccacf1cb19f31bfb6015bd029
Pavel Zhuravlev <propz@mail.ru>
2022-06-04 13:43:30 -0500

    update the box.go file

--- box.go
+++ box.go
@@ -28,7 +28,6 @@ func (b *box) AddShape(shape Shape) error {
 	b.shapes = append(b.shapes, shape)
 
 	return EmptyError{Message: ""}
-
 }
 
 // GetByIndex allows getting shape by index
@@ -46,7 +45,6 @@ func (b *box) GetByIndex(i int) (Shape, error) {
 	s := b.shapes[i]
 
 	return s, EmptyError{Message: ""}
-
 }
 
 // ExtractByIndex allows getting shape by index and removes this shape from the list.
@@ -57,7 +55,7 @@ func (b *box) ExtractByIndex(i int) (Shape, error) {
 
 	defer func() {
 		if err := recover(); err != nil {
-			fmt.Printf("error GetByIndex: %q\n", err)
+			fmt.Printf("error of ExtractByIndex: %q\n", err)
 		}
 	}()
 
@@ -68,31 +66,66 @@ func (b *box) ExtractByIndex(i int) (Shape, error) {
 	b.shapes = b.shapes[:len(b.shapes)-1]
 
 	return sh, EmptyError{Message: ""}
-
 }
 
 // ReplaceByIndex allows replacing shape by index and returns removed shape.
 // whether shape by index doesn't exist or index went out of the range, then it returns an error
 func (b *box) ReplaceByIndex(i int, shape Shape) (Shape, error) {
-	panic("implement me")
+	//panic("implement me")
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Printf("error of ReplaceByIndex: %q\n", err)
+		}
+	}()
 
+	sh := b.shapes[i]
+
+	b.shapes[i] = shape
+
+	return sh, EmptyError{Message: ""}
 }
 
 // SumPerimeter provides sum perimeter of all shapes in the list.
 func (b *box) SumPerimeter() float64 {
-	panic("implement me")
+	//panic("implement me")
 
+	var per float64 = 0.0
+
+	for _, sh := range b.shapes {
+
+		per += sh.CalcPerimeter()
+
+	}
+
+	return per
 }
 
 // SumArea provides sum area of all shapes in the list.
 func (b *box) SumArea() float64 {
-	panic("implement me")
+	//panic("implement me")
 
+	var area float64 = 0.0
+
+	for _, sh := range b.shapes {
+
+		area += sh.CalcArea()
+
+	}
+
+	return area
 }
 
 // RemoveAllCircles removes all circles in the list
 // whether circles are not exist in the list, then returns an error
 func (b *box) RemoveAllCircles() error {
-	panic("implement me")
+	//panic("implement me")
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Printf("error of RemoveAllCircles: %q\n", err)
+		}
+	}()
 
+	b.shapes = nil
+
+	return EmptyError{Message: ""}
 }
